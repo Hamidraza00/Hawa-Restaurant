132607296609a752659d5729c87e8869622ebbcd
maher <maher@HAMID>
2022-06-29 03:18:00 +0500

    Add project files.

---
+++ WebApplication4/Controllers/Home.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class Home : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            return View();
+        }
+
+        public IActionResult Menu()
+
+        {
+            return View();
+        }
+
+    public IActionResult Contact()
+    {
+        return View();
+    }
+}
+}
